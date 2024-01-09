15622d1f6ef8628551c4d0d6019e31c86f54e073
Aleena Beegum <aleena.anas90@gmail.com>
2024-01-09 11:54:57 +0530

    again commit login

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let conPassword=document.getElementById('floatingConPassword');
 let btn1=document.getElementById('btnsub');
 
 const isEmail = /^[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
-
+// Signup validation
 btn1.addEventListener('click', () => {
     if (fullname.value.trim() === '') {
         alert('Please enter fullname');
@@ -29,6 +29,32 @@ btn1.addEventListener('click', () => {
         password.value="";
         conPassword.value="";
         // document.getElementById('form-signup').reset();
+        
+    }
+});
+
+// Login validation
+
+let email1=document.getElementById('floatingInput1');
+let password1=document.getElementById('floatingPassword1');
+let btn2=document.getElementById('btnsub1');
+
+// Signup validation
+btn2.addEventListener('click', () => {
+    if (email1.value.trim() === '') {
+        alert('Please enter email');
+    } 
+    
+    else if (password1.value.trim() === '') {
+        alert('Please enter password');
+    } 
+    
+    else {
+        alert('Success');
+        
+        email1.value="";
+        password1.value="";
+        
         
     }
 });
